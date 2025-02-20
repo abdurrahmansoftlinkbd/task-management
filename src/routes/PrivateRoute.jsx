9d66dd46ff049fffb81604ab575fc346b20fd116
abdurrahmansoftlinkbd <kids.hasib.softlinkbd@gmail.com>
2025-02-20 23:07:52 +0600

    added privateRoute

---
+++ src/routes/PrivateRoute.jsx
@@ -0,0 +1,24 @@
+import { useContext } from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import PropTypes from "prop-types";
+import Loading from "../pages/Loading";
+import AuthContext from "../context/AuthContext";
+
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+  const location = useLocation();
+  if (loading) {
+    return <Loading></Loading>;
+  }
+  if (user && user?.email) {
+    return children;
+  }
+
+  return <Navigate state={location.pathname} to="/login"></Navigate>;
+};
+
+PrivateRoute.propTypes = {
+  children: PropTypes.object,
+};
+
+export default PrivateRoute;
