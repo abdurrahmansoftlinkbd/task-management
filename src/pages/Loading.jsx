7b77cbc441d6c0244c9d3e27d96d19f14981d083
abdurrahmansoftlinkbd <kids.hasib.softlinkbd@gmail.com>
2025-02-20 23:10:46 +0600

    added loading spinner

---
+++ src/pages/Loading.jsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <span className="loading loading-spinner loading-lg text-gray-600"></span>
+    </div>
+  );
+};
+
+export default Loading;
