26af1e7e5d3a3c38304cea2cd77af5ae4dfcb771
abdurrahmansoftlinkbd <kids.hasib.softlinkbd@gmail.com>
2025-02-20 23:05:51 +0600

    added authProvider

---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,83 @@
+import { useEffect, useState } from "react";
+import AuthContext from "./AuthContext";
+import PropTypes from "prop-types";
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import auth from "../firebase/firebase.init";
+import toast from "react-hot-toast";
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const googleProvider = new GoogleAuthProvider();
+
+  const handleGoogleSignIn = () => {
+    return signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+      })
+      .catch((error) => {
+        toast.error(`${error.code}`);
+      });
+  };
+
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const userLogin = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const updateUserProfile = (updatedData) => {
+    return updateProfile(auth.currentUser, updatedData);
+  };
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => {
+      unSubscribe();
+    };
+  }, []);
+
+  const logOut = () => {
+    setLoading(true);
+    toast.success("See you again soon!");
+    return signOut(auth);
+  };
+
+  const authInfo = {
+    user,
+    setUser,
+    loading,
+    createUser,
+    updateUserProfile,
+    handleGoogleSignIn,
+    userLogin,
+    logOut,
+  };
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+AuthProvider.propTypes = {
+  children: PropTypes.array,
+};
+
+export default AuthProvider;
