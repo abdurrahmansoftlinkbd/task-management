bb2431aeda8c48a65317280b8e4c7ffa0b9d7146
abdurrahmansoftlinkbd <kids.hasib.softlinkbd@gmail.com>
2025-02-22 12:03:14 +0600

    added success toast after add new task

--- src/components/AddTaskModal.jsx
+++ src/components/AddTaskModal.jsx
@@ -25,6 +25,7 @@ const AddTaskModal = ({ isAddingTask, setIsAddingTask, refetch }) => {
       });
       setIsAddingTask(false);
       refetch();
+      toast.success("Task added successfully");
     } catch (err) {
       toast.error(err?.message);
     }
